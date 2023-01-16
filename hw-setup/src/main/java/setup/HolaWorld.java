786d016ff435c257a4fe51eb7bb292e401baf845
taraki <tatsu@DESKTOP-02TROVO>
2023-01-16 14:04:52 -0800

    set up commit

--- hw-setup/src/main/java/setup/HolaWorld.java
+++ hw-setup/src/main/java/setup/HolaWorld.java
@@ -35,7 +35,7 @@ public class HolaWorld extends HelloWorld {
 
         // Print out greetings
         System.out.println(myFirstHW.getGreeting());
-        System.out.println(world);
+        System.out.println(world.getGreeting());
     }
 
     /**
