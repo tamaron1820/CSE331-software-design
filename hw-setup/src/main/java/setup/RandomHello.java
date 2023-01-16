f6323ea1ce2e9dd0fea10efe47759c78b509fe11
taraki <tatsu@DESKTOP-02TROVO>
2023-01-16 14:16:34 -0800

    add RandomHello

--- hw-setup/src/main/java/setup/RandomHello.java
+++ hw-setup/src/main/java/setup/RandomHello.java
@@ -1,4 +1,27 @@
 package setup;
-
+import java.util.Random;
 public class RandomHello {
+    /**
+     * Prints a random greeting to the console.
+     *
+     * @param args command-line arguments (ignored)
+     */
+    public static void main(String[] args) {
+        RandomHello randomHello = new RandomHello();
+        System.out.println(randomHello.getGreeting());
+    }
+
+    /** @return a greeting, randomly chosen from five possibilities */
+    public String getGreeting() {
+        // YOUR CODE GOES HERE
+        Random randomGenerator = new Random();
+        int num = randomGenerator.nextInt(5);
+        String[] greetings = new String[5];
+        greetings[0] = "Hello World";
+        greetings[1] = "Hola Mundo";
+        greetings[2] = "Bonjour Monde";
+        greetings[3] = "Hallo Welt";
+        greetings[4] = "Ciao Mondo";
+        return greetings[num];
+    }
 }
